2b622e2d32f448e02af69f9c310dbaae0ef73108
Jerry Jiang <jerry.jiang@discordapp.com>
2023-02-17 07:15:09 +0000

    No more waiting.

--- server/setup_startup.sh
+++ server/setup_startup.sh
@@ -4,6 +4,6 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "#!/bin/bash" > /etc/rc.local
 echo "/usr/bin/isticktoit_usb" >> /etc/rc.local
-echo "(sleep 10; sudo python3 $SCRIPTPATH/server.py) &" >> /etc/rc.local
+echo "sudo python3 $SCRIPTPATH/server.py &" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 chmod +x /etc/rc.local
